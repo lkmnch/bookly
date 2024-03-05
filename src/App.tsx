8737aaf2545887919681d52a867a439220c62136
Loukmane Chaou <72920928+lkmnch@users.noreply.github.com>
2024-03-05 23:26:32 +0100

    Add react-router-dom and @remix-run/router dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css"
+import Navigation from "./components/Navigation"
 
+//App wird mit react router nicht gebraucht einzelne Routen direkt in Main
 function App() {
 	return (
 		<>
 			<h1>Bookly</h1>
-
 			<p className='read-the-docs'>App for booking Seats in Restaurants.</p>
+			<Navigation />
 		</>
 	)
 }
